154da3d471ac5f58b0c28a56a0adef597f19ae1b
Ahmed Nabil <ahmednabilgm7@gmail.com>
2021-11-01 14:21:13 +0200

    add valu

--- libs/helpers/src/valu.service.ts
+++ libs/helpers/src/valu.service.ts
@@ -41,8 +41,8 @@ export class ValuService {
     return this.accessToken;
   }
   async enquiry(params: ValuEnquiryParams): Promise<string> {
-    this.generateToken();
-    const token = 'Bearer ' + this.accessToken;
+    // this.generateToken();
+    // const token = 'Bearer ' + this.accessToken;
     try {
       const response = await this.instance.post(
         'ECommerce/Inquiry',
@@ -64,7 +64,8 @@ export class ValuService {
         },
         {
           headers: {
-            Authorization: token,
+            Authorization:
+              'Bearer eyJraWQiOiJzc29zIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJlYmFjMTNiZi1iYjQ0LTQ1MTEtYjYzMi1jZWY2YzI0ODNkMDciLCJhdWQiOiJlYmFjMTNiZi1iYjQ0LTQ1MTEtYjYzMi1jZWY2YzI0ODNkMDciLCJpc3MiOiJBUElHV1Rva2VuSXNzdWVyIiwiQXBwbGljYXRpb25JZCI6IjYzZDU3NDhjLWNiNDEtNDBiMS1iMmI4LWE2MTg4ZTA5ZmExNyIsImV4cCI6MTYzNTc3MDEyMywiaWF0IjoxNjM1NzY5MjIzfQ.LRrhM1wO7ujW1clxYtj_eo-x6_-__e-23J1egD4xjfphIYr7-j0Gz1A1RYI3qx9K-IWOoz9Bbh2DAht8OlFwN8RT2tAZTDzDdN_Gb3c1Z_6JR0PV5mqa7ywgBd6PWbgc8kZe0Zt0HRt2hwpgvaSsdch4b6lKQovUb88lF4PGfIIAcMEn1Q2oG0jBxOJgCdiu67RNyeqY9eo4RJmDUlo80KOs3Or9cY4nV31iBO1dAQ9EU11nd3Ks-cBdaKUIrw35lyR8dCnSfeTYts-biBsVrlCUzqKpD83Z-jBoEZxUtKxAqJFBx2VRnqApuDZGItCZJuvh9e66gPW49mcC_Is1xw',
             'Transfer-Encoding': 'chunked',
           },
         },

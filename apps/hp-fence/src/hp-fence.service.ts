df78492f97ee39285e3445628570708c17a3cbcb
Ahmed Nabil <ahmednabilgm7@gmail.com>
2021-11-01 13:55:56 +0200

    add valu

--- apps/hp-fence/src/hp-fence.service.ts
+++ apps/hp-fence/src/hp-fence.service.ts
@@ -1,24 +1,24 @@
 import { ValuService } from '@app/helpers';
-import { ValuEnquiryParams } from '@app/helpers/helpers.types';
 import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HpFenceService {
   constructor(@Inject(ValuService) private valuService: ValuService) {}
   getHello() {
-    const enquiryParams: ValuEnquiryParams = {
-      mobileNumber: '00009981337',
-      productList: [
-        {
-          productId: 'EGMHOC23DP5',
-          productPrice: 500,
-          orderId: '8232569b800742fa8d01410e7ac79b45',
-          downPayment: 0,
-          ToUAmount: 0,
-          CashbackAmount: 0,
-        },
-      ],
-    };
-    return this.valuService.enquiry(enquiryParams);
+    // const enquiryParams: ValuEnquiryParams = {
+    //   mobileNumber: '00009981337',
+    //   productList: [
+    //     {
+    //       productId: 'EGMHOC23DP5',
+    //       productPrice: 500,
+    //       orderId: '8232569b800742fa8d01410e7ac79b45',
+    //       downPayment: 0,
+    //       ToUAmount: 0,
+    //       CashbackAmount: 0,
+    //     },
+    //   ],
+    // };
+    // return this.valuService.enquiry(enquiryParams);
+    return this.valuService.generateToken();
   }
 }

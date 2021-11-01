8377e713f5c4d0ad6c11965db75dc1cc966e38e2
Ahmed Nabil <ahmednabilgm7@gmail.com>
2021-11-01 14:26:56 +0200

    add valu

--- libs/helpers/src/valu.service.ts
+++ libs/helpers/src/valu.service.ts
@@ -44,34 +44,37 @@ export class ValuService {
     // this.generateToken();
     // const token = 'Bearer ' + this.accessToken;
     try {
-      const response = await this.instance.post(
-        'ECommerce/Inquiry',
-        {
-          aggregatorId: 'Jumia',
-          vendorId: 'VDR2610212937',
-          storeId: '121311661',
-          mobileNumber: '00009981337',
-          productList: [
-            {
-              productId: 'EGMHOC23DP5',
-              productPrice: 500,
-              orderId: '8232569b800742fa8d01410e7ac79b45',
-              downPayment: 0,
-              ToUAmount: 0,
-              CashbackAmount: 0,
-            },
-          ],
-        },
-        {
-          headers: {
-            Authorization:
-              'Bearer eyJraWQiOiJzc29zIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJlYmFjMTNiZi1iYjQ0LTQ1MTEtYjYzMi1jZWY2YzI0ODNkMDciLCJhdWQiOiJlYmFjMTNiZi1iYjQ0LTQ1MTEtYjYzMi1jZWY2YzI0ODNkMDciLCJpc3MiOiJBUElHV1Rva2VuSXNzdWVyIiwiQXBwbGljYXRpb25JZCI6IjYzZDU3NDhjLWNiNDEtNDBiMS1iMmI4LWE2MTg4ZTA5ZmExNyIsImV4cCI6MTYzNTc3MDEyMywiaWF0IjoxNjM1NzY5MjIzfQ.LRrhM1wO7ujW1clxYtj_eo-x6_-__e-23J1egD4xjfphIYr7-j0Gz1A1RYI3qx9K-IWOoz9Bbh2DAht8OlFwN8RT2tAZTDzDdN_Gb3c1Z_6JR0PV5mqa7ywgBd6PWbgc8kZe0Zt0HRt2hwpgvaSsdch4b6lKQovUb88lF4PGfIIAcMEn1Q2oG0jBxOJgCdiu67RNyeqY9eo4RJmDUlo80KOs3Or9cY4nV31iBO1dAQ9EU11nd3Ks-cBdaKUIrw35lyR8dCnSfeTYts-biBsVrlCUzqKpD83Z-jBoEZxUtKxAqJFBx2VRnqApuDZGItCZJuvh9e66gPW49mcC_Is1xw',
-            'Transfer-Encoding': 'chunked',
+      const response = this.instance
+        .post(
+          'ECommerce/Inquiry',
+          {
+            aggregatorId: 'Jumia',
+            vendorId: 'VDR2610212937',
+            storeId: '121311661',
+            mobileNumber: '00009981337',
+            productList: [
+              {
+                productId: 'EGMHOC23DP5',
+                productPrice: 500,
+                orderId: '8232569b800742fa8d01410e7ac79b45',
+                downPayment: 0,
+                ToUAmount: 0,
+                CashbackAmount: 0,
+              },
+            ],
           },
-        },
-      );
-      console.log('[ValuService.enquiry]', response.data);
-      return String(response.data);
+          {
+            headers: {
+              Authorization:
+                'Bearer eyJraWQiOiJzc29zIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJlYmFjMTNiZi1iYjQ0LTQ1MTEtYjYzMi1jZWY2YzI0ODNkMDciLCJhdWQiOiJlYmFjMTNiZi1iYjQ0LTQ1MTEtYjYzMi1jZWY2YzI0ODNkMDciLCJpc3MiOiJBUElHV1Rva2VuSXNzdWVyIiwiQXBwbGljYXRpb25JZCI6IjYzZDU3NDhjLWNiNDEtNDBiMS1iMmI4LWE2MTg4ZTA5ZmExNyIsImV4cCI6MTYzNTc3MDEyMywiaWF0IjoxNjM1NzY5MjIzfQ.LRrhM1wO7ujW1clxYtj_eo-x6_-__e-23J1egD4xjfphIYr7-j0Gz1A1RYI3qx9K-IWOoz9Bbh2DAht8OlFwN8RT2tAZTDzDdN_Gb3c1Z_6JR0PV5mqa7ywgBd6PWbgc8kZe0Zt0HRt2hwpgvaSsdch4b6lKQovUb88lF4PGfIIAcMEn1Q2oG0jBxOJgCdiu67RNyeqY9eo4RJmDUlo80KOs3Or9cY4nV31iBO1dAQ9EU11nd3Ks-cBdaKUIrw35lyR8dCnSfeTYts-biBsVrlCUzqKpD83Z-jBoEZxUtKxAqJFBx2VRnqApuDZGItCZJuvh9e66gPW49mcC_Is1xw',
+              'Transfer-Encoding': 'chunked',
+            },
+          },
+        )
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+      // console.log('[ValuService.enquiry]', response.data);
+      return String('');
     } catch (e) {
       return String(e.message + ' ' + e.code);
     }

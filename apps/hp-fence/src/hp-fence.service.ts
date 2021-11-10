72894cda1eaf4badb353d4bd33f19d686c01a319
Ahmed Nabil <ahmednabilgm7@gmail.com>
2021-11-10 15:33:52 +0200

    fix 2

--- apps/hp-fence/src/hp-fence.service.ts
+++ apps/hp-fence/src/hp-fence.service.ts
@@ -16,7 +16,7 @@ export class HpFenceService {
         {
           productId: 'EGMHOC23DP5',
           productPrice: 500,
-          orderId: '8232569b800742fa8d01410e7ac79b45',
+          orderId: '8232569b800742fa8d01410e7ac79b46',
           downPayment: 0,
           ToUAmount: 0,
           CashbackAmount: 0,
@@ -26,7 +26,7 @@ export class HpFenceService {
 
     const verifyParams: ValuVerifyCustomerParams = {
       mobileNumber: '00009981337',
-      orderId: '8232569b800742fa8d01410e7ac79b45',
+      orderId: '8232569b800742fa8d01410e7ac79b46',
     };
 
     const purchaseParams: ValuPurchaseParams = {
@@ -36,7 +36,7 @@ export class HpFenceService {
         {
           productId: 'EGMHOC23DP5',
           productPrice: 500,
-          orderId: '8232569b800742fa8d01410e7ac79b45',
+          orderId: '8232569b800742fa8d01410e7ac79b46',
           downPayment: 0,
           ToUAmount: 0,
           CashbackAmount: 0,

fbd2c9ad47cdd71e042095aed32607d17b64aea5
Ahmed Nabil <ahmednabilgm7@gmail.com>
2021-11-11 14:35:32 +0200

    fix 2

--- apps/hp-fence/src/main.ts
+++ apps/hp-fence/src/main.ts
@@ -3,6 +3,7 @@ import { HpFenceModule } from './hp-fence.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(HpFenceModule);
+  app.enableCors();
   await app.listen(3002);
 }
 bootstrap();

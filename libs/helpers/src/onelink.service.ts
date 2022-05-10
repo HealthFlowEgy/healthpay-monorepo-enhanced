9823a9680c1b0bfb72e9d8d59b82088076f6b56c
AhmedNabil <ahmednabilgm7@gmail.com>
2022-05-10 16:02:45 +0200

    test

--- libs/helpers/src/onelink.service.ts
+++ libs/helpers/src/onelink.service.ts
@@ -21,30 +21,29 @@ export class OnelinkService {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
+        'x-api-key': '52206e2a-09a6-4ef9-b69f-f286a802b105',
       },
     });
   }
 
   private async getAccessToken() {
-    const response = await this.instance.post('/login', {
-      username: this.configService.get<string>('ONELINK_USERNAME'),
-      password: this.configService.get<string>('ONELINK_PASSWORD'),
+    const response = await this.instance.post('/auth/login', {
+      api_key: '52206e2a-09a6-4ef9-b69f-f286a802b105',
+      api_secret: '13008cc3-e996-4c55-ab50-a2779e4027d4',
     });
+    console.log(response.data);
     this.access_token = response.data.token;
   }
 
-  async createTransaction(
-    user: User,
-    amount: number,
-  ): Promise<OnelinkTransactionResponse> {
+  async createTransaction(user: User, amount: number): Promise<any> {
     try {
       if (!this.access_token) {
         await this.getAccessToken();
       }
+
       const data = {
-        token: this.access_token,
         first_name: ' ' + user.firstName + ' ',
-        last_name: ' ' + user.firstName + user.lastName + ' ',
+        last_name: user.lastName ?? " ",
         mobile: user.mobile.replace('+2', ''),
         email: user.email || 'info@cloudx9.io',
         amount,
@@ -52,11 +51,21 @@ export class OnelinkService {
       const transactionRes = await this.instance.post(
         '/transaction/create',
         data,
+        {
+          headers: {
+            Authorization: `Bearer ${this.access_token}`,
+          },
+        },
       );
       this.logger.verbose(
         `[createTransaction], ${JSON.stringify(transactionRes.data)}`,
       );
-      return transactionRes.data;
+      // return transactionRes.data;
+      this.logger.verbose(transactionRes.data.uid);
+      return {
+        transaction_id: transactionRes.data.uid,
+        iframe: transactionRes.data.iframe_url,
+      };
     } catch (e) {
       this.logger.verbose(
         `[createTransactionError], ${JSON.stringify(e.response.data || e)}`,

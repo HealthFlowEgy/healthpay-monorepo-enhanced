c9270b914b6888f2251cf6f856e540732f9e0c66
AhmedAshraf <ahmed.ashraf@cloudx9.io>
2022-06-15 18:59:13 +0200

    add rate limiting

--- apps/hp-sword/src/guards/throttle.guard.ts
+++ apps/hp-sword/src/guards/throttle.guard.ts
@@ -11,8 +11,9 @@ import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
-  getTracker(req: Record<string, any>): string {
-    console.log('[GqlThrottlerGuard.getTracker]', req.ip, req.ips);
+  protected getTracker(req: Record<string, any>): string {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;;
+    console.log('[GqlThrottlerGuard.getTracker]', req.ip, req.ips , ip);
     return req.ips.length ? req.ips[0] : req.ip;
   }
 

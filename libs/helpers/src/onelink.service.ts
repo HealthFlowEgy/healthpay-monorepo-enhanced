608c1c38d0f32fa6443f54b83c2879b29ef24cf0
AhmedAshraf <ahmed.ashraf@cloudx9.io>
2022-08-16 16:14:30 +0200

    fix includes error

--- libs/helpers/src/onelink.service.ts
+++ libs/helpers/src/onelink.service.ts
@@ -21,15 +21,15 @@ export class OnelinkService {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        'x-api-key': '8301df53-4ca9-465b-9878-a134c4a17942',
+        'x-api-key': '52206e2a-09a6-4ef9-b69f-f286a802b105',
       },
     });
   }
 
   private async getAccessToken() {
     const response = await this.instance.post('/auth/login', {
-      api_key: '8301df53-4ca9-465b-9878-a134c4a17942',
-      api_secret: '28cb42c5-01d9-4c09-98f8-e1e276497a52',
+ api_key: '52206e2a-09a6-4ef9-b69f-f286a802b105',
+ api_secret: '13008cc3-e996-4c55-ab50-a2779e4027d4',
     });
     console.log(response.data);
     this.access_token = response.data.token;
